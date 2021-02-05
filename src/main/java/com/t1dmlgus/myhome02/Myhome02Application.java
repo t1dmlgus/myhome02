6b2a5adcb753ebac4f952ec28bf97b5668336a5f
t1dmlgus <dmlgusgngl@gmail.com>
2021-02-05 11:43:42 +0900

    init

---
+++ src/main/java/com/t1dmlgus/myhome02/Myhome02Application.java
@@ -0,0 +1,13 @@
+package com.t1dmlgus.myhome02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myhome02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myhome02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/t1dmlgus/myhome02/Myhome02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1dmlgus.myhome02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myhome02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
